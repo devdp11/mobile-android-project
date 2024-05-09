997d284900ca4bc4624d85ad71c9b57da3f0e9bd
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-05-09 19:44:28 +0100

    refactored code

--- app/src/main/java/com/example/android_studio_project/activity/LoginActivity.kt
+++ app/src/main/java/com/example/android_studio_project/activity/LoginActivity.kt
@@ -1,16 +1,17 @@
-package com.example.android_studio_project
+package com.example.android_studio_project.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_studio_project.R
 
-class register : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/android_studio_project/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/android_studio_project/activity/RegisterActivity.kt
@@ -1,16 +1,17 @@
-package com.example.android_studio_project
+package com.example.android_studio_project.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_studio_project.R
 
-class login : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
