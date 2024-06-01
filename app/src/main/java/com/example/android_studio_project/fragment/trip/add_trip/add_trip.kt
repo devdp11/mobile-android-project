df8ec918f7a4e3f0889411d190dbe6200b8abf25
Nx-7 <96266508+Nx-7@users.noreply.github.com>
2024-06-01 14:57:26 +0100

    Add_trip e o fragmento

--- app/src/main/java/com/example/android_studio_project/fragment/trip/add_trip/add_trip.kt
+++ app/src/main/java/com/example/android_studio_project/fragment/trip/add_trip/add_trip.kt
@@ -1,32 +1,32 @@
 package com.example.android_studio_project.fragment.trip.add_trip
 
-import android.app.Activity
-import android.app.DatePickerDialog
-import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
+import android.content.Context
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.android_studio_project.R
-import java.util.Calendar
+import com.example.android_studio_project.data.retrofit.models.TripModel
+import com.example.android_studio_project.data.retrofit.services.TripService
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class AddTripFragment : Fragment(), CustomDatePickerFragment.OnDateSelectedListener {
+class AddTripFragment : Fragment() {
 
     private lateinit var tripNameEditText: EditText
     private lateinit var tripDateEditText: EditText
+    private lateinit var tripDescriptionEditText: EditText
     private lateinit var tripRatingBar: RatingBar
-    private lateinit var addPhotosButton: Button
-    private lateinit var photosGridView: GridView
-    private lateinit var saveTripButton: Button
 
-    private val photosList = mutableListOf<Uri>()
-    private lateinit var photosAdapter: PhotosAdapter
+    private lateinit var saveTripButton: Button
+    private lateinit var tripService: TripService
+
+    private lateinit var tripStartDate: Date
+    private lateinit var tripEndDate: Date
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,67 +37,77 @@ class AddTripFragment : Fragment(), CustomDatePickerFragment.OnDateSelectedListe
         tripNameEditText = view.findViewById(R.id.trip_name)
         tripDateEditText = view.findViewById(R.id.trip_date)
         tripDateEditText.setOnClickListener {
-            val datePickerFragment = CustomDatePickerFragment()
-            datePickerFragment.listener = this
-            datePickerFragment.show(parentFragmentManager, "datePicker")
+            showDatePicker()
         }
+        tripDescriptionEditText = view.findViewById(R.id.trip_description)
         tripRatingBar = view.findViewById(R.id.trip_rating)
-        addPhotosButton = view.findViewById(R.id.add_photos_button)
-        photosGridView = view.findViewById(R.id.photos_grid)
         saveTripButton = view.findViewById(R.id.save_trip_button)
-
-        photosAdapter = PhotosAdapter()
-        photosGridView.adapter = photosAdapter
-
-        addPhotosButton.setOnClickListener {
-            openGallery()
-        }
-
         saveTripButton.setOnClickListener {
             saveTrip()
         }
+        tripService = TripService()
 
         return view
-    }
-
-    private val selectPhotosLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uris: List<Uri> ->
-        photosList.addAll(uris)
-        photosAdapter.notifyDataSetChanged()
-    }
-
-    private fun openGallery() {
-        selectPhotosLauncher.launch("image/*")
     }
 
     private fun saveTrip() {
         val tripName = tripNameEditText.text.toString()
         val tripDate = tripDateEditText.text.toString()
+        val tripDescription = tripDescriptionEditText.text.toString()
         val tripRating = tripRatingBar.rating
 
-        // bade de dados aqui
-        Toast.makeText(context, "Trip Saved: $tripName, Date: $tripDate, Rating: $tripRating", Toast.LENGTH_LONG).show()
-    }
+        val tripStartDate = this.tripStartDate
+        val tripEndDate = this.tripEndDate
 
-    inner class PhotosAdapter : BaseAdapter() {
-        override fun getCount(): Int = photosList.size
+        if (tripName.isNotEmpty() && tripDate.isNotEmpty() && tripStartDate != null && tripEndDate != null) {
+            val trip = TripModel(
+                description = tripDescription,
+                name = tripName,
+                startDate = tripStartDate,
+                endDate = tripEndDate,
+                rating = tripRating,
+            )
 
-        override fun getItem(position: Int): Uri = photosList[position]
-
-        override fun getItemId(position: Int): Long = position.toLong()
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val imageView = convertView as? ImageView ?: ImageView(requireContext()).apply {
-                layoutParams = AbsListView.LayoutParams(300, 300)
-                scaleType = ImageView.ScaleType.CENTER_CROP
-            }
-
-            imageView.setImageURI(getItem(position))
-            return imageView
+            tripService.createTrip(trip, onResponse = { responseMessage ->
+                requireActivity().runOnUiThread {
+                    if (responseMessage == "success") {
+                        Toast.makeText(context, "Trip Saved", Toast.LENGTH_LONG).show()
+                        // Optionally, navigate back or clear the form
+                    } else {
+                        Toast.makeText(context, "Error saving trip", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }, onFailure = { throwable ->
+                requireActivity().runOnUiThread {
+                    Toast.makeText(context, "Error: ${throwable.message}", Toast.LENGTH_LONG).show()
+                }
+            })
+        } else {
+            Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onDateSelected(year: Int, month: Int, day: Int) {
-        val selectedDate = "$day/${month + 1}/$year"
-        tripDateEditText.setText(selectedDate)
+    private fun showDatePicker() {
+        val datePicker = MaterialDatePicker.Builder.dateRangePicker()
+            .setTitleText("Select date range")
+            .setTheme(R.style.ThemeOverlay_App_DatePicker)
+            .build()
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            tripStartDate = Date(selection.first ?: 0)
+            tripEndDate = Date(selection.second ?: 0)
+            val startDate = dateFormat.format(tripStartDate)
+            val endDate = dateFormat.format(tripEndDate)
+            val dateRange = "$startDate - $endDate"
+            tripDateEditText.setText(dateRange)
+        }
+
+        datePicker.show(parentFragmentManager, "datePicker")
+    }
+
+    private fun getLoggedUserId(): Int {
+        val sharedPreferences = requireActivity().getSharedPreferences("UserLoggedPrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getInt("userId", -1)
     }
 }
