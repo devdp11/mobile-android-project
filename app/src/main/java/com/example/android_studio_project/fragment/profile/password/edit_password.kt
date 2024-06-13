3af782e15e2e92dd511b5059a93497b4c4f87909
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-06-13 15:41:26 +0100

    created logic on update password

--- app/src/main/java/com/example/android_studio_project/fragment/profile/password/edit_password.kt
+++ app/src/main/java/com/example/android_studio_project/fragment/profile/password/edit_password.kt
@@ -1,6 +1,8 @@
 package com.example.android_studio_project.fragment.profile.password
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.android_studio_project.R
 import com.example.android_studio_project.data.retrofit.services.AuthService
@@ -31,14 +34,52 @@ class edit_password(private val userEmail: String) : Fragment() {
         val newPasswordEditText = view.findViewById<EditText>(R.id.new_password)
         val confirmNewPasswordEditText = view.findViewById<EditText>(R.id.confirm_password)
 
+        val passwordRequirementsTextOne = view.findViewById<TextView>(R.id.password_requirements_text_one)
+        val passwordRequirementsTextTwo = view.findViewById<TextView>(R.id.password_requirements_text_two)
+        val passwordRequirementsTextThree = view.findViewById<TextView>(R.id.password_requirements_text_three)
+        val passwordRequirementsTextFour = view.findViewById<TextView>(R.id.password_requirements_text_four)
+
         val backButton: ImageView = view.findViewById(R.id.btn_back)
         backButton.setOnClickListener {
             requireActivity().onBackPressed()
         }
 
+        newPasswordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val password = s.toString()
+
+                if (password.any { it.isUpperCase() }) {
+                    passwordRequirementsTextOne.setTextColor(resources.getColor(R.color.green))
+                } else {
+                    passwordRequirementsTextOne.setTextColor(resources.getColor(R.color.red))
+                }
+
+                if (password.any { it.isLowerCase() }) {
+                    passwordRequirementsTextTwo.setTextColor(resources.getColor(R.color.green))
+                } else {
+                    passwordRequirementsTextTwo.setTextColor(resources.getColor(R.color.red))
+                }
+
+                if (password.any { it.isDigit() }) {
+                    passwordRequirementsTextThree.setTextColor(resources.getColor(R.color.green))
+                } else {
+                    passwordRequirementsTextThree.setTextColor(resources.getColor(R.color.red))
+                }
+
+                if (password.any { "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~".contains(it) }) {
+                    passwordRequirementsTextFour.setTextColor(resources.getColor(R.color.green))
+                } else {
+                    passwordRequirementsTextFour.setTextColor(resources.getColor(R.color.red))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
         val updateButton = view.findViewById<Button>(R.id.save_btn)
         updateButton.setOnClickListener {
-
             val oldPassword = oldPasswordEditText.text.toString()
             val newPassword = newPasswordEditText.text.toString()
             val confirmNewPassword = confirmNewPasswordEditText.text.toString()
