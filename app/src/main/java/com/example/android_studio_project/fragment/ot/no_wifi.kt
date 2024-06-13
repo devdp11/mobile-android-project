994a84214172cde46eff4eb359891ee7eb94cfb1
kromenz <rafael.pires.andre@ipvc.pt>
2024-06-13 19:55:10 +0100

    listener of no internet connection, and some more adjustments

---
+++ app/src/main/java/com/example/android_studio_project/fragment/ot/no_wifi.kt
@@ -0,0 +1,18 @@
+package com.example.android_studio_project.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android_studio_project.R
+
+class no_wifi : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.no_internet, container, false)
+    }
+}
